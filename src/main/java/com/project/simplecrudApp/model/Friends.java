9d80cc3a5f739bf3a98db463a7ff865606a7a3a2
lathief <shadowmaster241099@yahoo.co.id>
2022-12-21 14:09:07 +0700

    edit entity

--- src/main/java/com/project/simplecrudApp/model/Friends.java
+++ src/main/java/com/project/simplecrudApp/model/Friends.java
@@ -1,4 +1,20 @@
 package com.project.simplecrudApp.model;
 
+import lombok.Data;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class Friends {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstname;
+    private String lastname;
+
 }
